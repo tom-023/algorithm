c7b6c3f7520ca4feb807d4eb4677c0af5eaeabac
tom023 <guanlidubian@gmail.com>
2022-10-02 14:59:30 +0900

    goutils

--- bubbleSort/main.go
+++ bubbleSort/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alok87/goutils/pkg/random"
+)
 
 func main() {
-	list := []int{6, 34, 12, 2, 35, 3, 8, 4, 15, 31, 60, 54}
+	// list := []int{6, 34, 12, 2, 35, 3, 8, 4, 15, 31, 60, 54}
+	list := random.RangeInt(2, 100, 5)
 	bubbleSort(list...)
 	fmt.Printf("%v\n", list)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module tom-023/algorithm
+
+go 1.18
+
+require github.com/alok87/goutils v0.0.0-20170710173822-29ef704e5505
